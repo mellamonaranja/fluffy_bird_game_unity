4340749f78e99452878d8dfcafc311a4b8971a39
Joohyun Yoon <joohyunyoon@Joohyuns-MacBook-Pro.local>
2023-02-08 18:32:09 +0100

    added files and pics

---
+++ LogicScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public Text scoreText;
+    public GameObject GameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore=playerScore+scoreToAdd;
+        scoreText.text=playerScore.ToString();
+        Debug.Log("scoreText.text: " + scoreText.text);
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        GameOverScreen.SetActive(true);
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# fluffy_bird_game_unity
